d2546d53807baec51a530fc669a69007e263ba47
Manuel Brestak <manuel.brestak@gmail.com>
2022-04-05 14:44:54 +0200

    27. Creating Links

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutIconLink from './components/AboutIconLink';
 import FeedbackData from './data/FeedbackData';
 import AboutPage from './pages/AboutPage';
 
@@ -43,6 +44,8 @@ function App() {
           ></Route>
           <Route path="/about" element={<AboutPage />} />
         </Routes>
+
+        <AboutIconLink />
       </div>
     </Router>
   );
